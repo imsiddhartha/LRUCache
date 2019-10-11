daec5485dee0fe2c2247046fd64fcf723ffb16d7
imsiddhartha <siddharth2408@gmail.com>
2019-10-11 22:41:39 +0530

    Adding problem statement.

--- LRUCache.cpp
+++ LRUCache.cpp
@@ -1,12 +1,20 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
 
-
-/*Problem Statement: https://leetcode.com/problems/lru-cache/*/
-
 /**
 * This code is added and uploaded to github by Siddharth Saklecha and is licensed under the Apache 2.0 license - see https://www.apache.org/licenses/LICENSE-2.0
-* Written by: Siddharth Saklecha, https://github.com/imsiddhartha
+* Owner: Siddharth Saklecha, https://github.com/imsiddhartha
+*/
+
+
+/*Problem Statement from: https://leetcode.com/problems/lru-cache/*/
+/*
+ *  Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+ *  get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ *  put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+ *  The cache is initialized with a positive capacity.
 */
 
 using namespace std;
